fe72b852e20ceba4bf720d1e07e800d5182cfc2b
tuonybt <tonybt.tbt@gmail.com>
2023-05-01 16:22:48 +0100

    Find all offs for each on

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::{BTreeMap};
+use itertools::iproduct;
 
 fn main() {
 
-    let mut dcs = Vec::<[usize; 2]>::new();
     let mut ons = BTreeMap::<usize, Vec<usize>>::new();
     let mut offs = BTreeMap::<usize, Vec<usize>>::new();
 
@@ -10,20 +10,28 @@ fn main() {
         for off in on + 1 .. 41 {
             let dc = on as f64 / (on as f64 + off as f64) * 100.0;
             if dc % 1.0 == 0.0 {
-                dcs.push([on, off]);
                 ons.entry(on).or_insert(Vec::new()).push(off);
                 offs.entry(off).or_insert(Vec::new()).push(on);
             }
 
         }
     }
-        //println!("{:?}", dcs);
-        println!("{:?}", ons.keys());
-        //println!("{:?}", offs);
-        for on in &ons {
-            let check_contiguous = (1..4).map(|z| ons.contains_key(&(on.0 + z))).collect::<Vec<bool>>();
-            if !check_contiguous.contains(&false) {
-                println!("{:?}", on);
+    //println!("{:?}", ons.keys());
+    //println!("{:?}", offs);
+    for on in &ons {
+        let check_contiguous = (1..4).map(|z| ons.contains_key(&(on.0 + z))).collect::<Vec<bool>>();
+        if !check_contiguous.contains(&false) {
+            let all_offs = (0usize..4).map(|z| ons[&(on.0 + z)].to_owned()).collect::<Vec<Vec<usize>>>();
+            for a in &all_offs[0] {
+                for b in &all_offs[0] {
+                    for c in &all_offs[0] {
+                        for d in &all_offs[0] {
+                            println!("{:?} {:?}", on.0, vec![a, b, c, d]);
+
+                        }
+                    }
+                }
             }
         }
+    }
 }
