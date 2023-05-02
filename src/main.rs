0c244f306a6442843f0ebc2c19d8d32d9c89e1db
tuonybt <tonybt.tbt@gmail.com>
2023-05-02 07:43:03 +0100

    Solution Expressed as Duty Cycles

--- src/main.rs
+++ src/main.rs
@@ -17,38 +17,42 @@ fn main() {
 
         }
     }
-    //println!("{:?}", ons.keys());
-    //println!("{:?}", offs);
-    for on in &ons {
-        let check_contiguous = (1..4).map(|z| ons.contains_key(&(on.0 + z))).collect::<Vec<bool>>();
-        if !check_contiguous.contains(&false) {
-            let all_offs = (0usize..4).map(|z| ons[&(on.0 + z)].to_owned()).collect::<Vec<Vec<usize>>>();
-            if on.0 == &11usize {
-                println!("{:?}", all_offs);
-            }
-            for (a, b, c, d) in iproduct!(&all_offs[0], &all_offs[1], &all_offs[2], &all_offs[3]) {
-                    let on_count = [a, b, c, d].into_iter().collect::<Counter<&usize>>();
-                    if on_count.len() == 2 {
-                        println!("{:?} {:?}", on.0, on_count);
 
-                    }
+    let on_group = find_pairs(&ons);
+    if on_group.len() == 1 {
+        let duty_times = on_group[0].iter()
+        .map(|z| z.0 * 100 / (z.0 + z.1))
+        .collect::<Vec<usize>>();
+        println!("First set duty times: {:?}", duty_times);
+    } 
+
+    let off_group = find_pairs(&offs);
+    if off_group.len() == 1 {
+        let duty_times = off_group[0].iter()
+        .map(|z| z.1 * 100 / (z.0 + z.1))
+        .collect::<Vec<usize>>();
+        println!("Second set duty times: {:?}", duty_times);
+    } 
+
+
+}
+
+fn find_pairs(state_times: &BTreeMap::<usize, Vec<usize>>) -> Vec<Vec<(usize, usize)>> {
+    let mut stps = Vec::<Vec<(usize, usize)>>::new();
+    for st in state_times {
+        let check_contiguous = (1..4).map(|z| state_times.contains_key(&(st.0 + z))).collect::<Vec<bool>>();
+        if !check_contiguous.contains(&false) {
+            let all_comps = (0usize..4).map(|z| state_times[&(st.0 + z)].to_owned()).collect::<Vec<Vec<usize>>>();
+            for (a, b, c, d) in iproduct!(&all_comps[0], &all_comps[1], &all_comps[2], &all_comps[3]) {
+                let st_count = [a, b, c, d].into_iter().collect::<Counter<&usize>>();
+                if st_count.values().collect::<Vec<&usize>>() == vec![&2, &2] {
+                    let state_pairs = (0usize..4).map(|z| (st.0 + z)).collect::<Vec<usize>>();
+                    stps.push(state_pairs.iter().zip([a, b, c, d])
+                    .map(|(&x, &y)| (x, y))
+                    .collect::<Vec<(usize, usize)>>());
+                }
             }
         }
     }
-
-    for on in &offs {
-        let check_contiguous = (1..4).map(|z| offs.contains_key(&(on.0 + z))).collect::<Vec<bool>>();
-        if !check_contiguous.contains(&false) {
-            let all_offs = (0usize..4).map(|z| offs[&(on.0 + z)].to_owned()).collect::<Vec<Vec<usize>>>();
-            for (a, b, c, d) in iproduct!(&all_offs[0], &all_offs[1], &all_offs[2], &all_offs[3]) {
-                    let on_count = [a, b, c, d].into_iter().collect::<Counter<&usize>>();
-                    if on_count.len() == 2 {
-                        println!("{:?} {:?}", on.0, on_count);
-
-                    }
-            }
-        }
-    }
-
-
+    stps
 }
